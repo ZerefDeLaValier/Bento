3b91b7cb5ebaa88a7d66efdcceb687d41490e42d
ZerefDeLaValier <37865998+ZerefDeLaValier@users.noreply.github.com>
2022-03-10 11:06:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,25 +11,26 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'ZerefDeLaValier',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: 'Доброе утро!',
+  greetingAfternoon: 'Добрый день,',
+  greetingEvening: 'Добрый вечер,',
+  greetingNight: 'Погнали спать!',
+
 
   // Layout
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: '1f510d6075ad900912b6347562801b36', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  language: 'ru', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
   defaultLatitude: '37.775',
